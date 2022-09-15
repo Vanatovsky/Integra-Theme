1388e062af449a7a8d91fdd4a263787219c29924
Vanatovsky <taras1000000000@gmail.com>
2022-09-15 11:38:51 +0300

    Objects with breadcrumbs

---
+++ wp-content/themes/rns/archive-object.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * The template for displaying archive pages
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Run_Fun
+ */
+
+get_header();
+?>
+
+<main id="primary" class="site-main">
+
+	<?php
+	$post_page = get_page_by_path('object', OBJECT, 'page');
+	echo SiteOrigin_Panels::renderer()->render($post_page->ID);
+	?>
+
+</main><!-- #main -->
+
+<?php
+get_sidebar();
+get_footer();
